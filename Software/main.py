6ba53eb4203fe3757b6fda51e178c08acb5a25b2
lolzmon <lolzzmon@gmail.com>
2026-07-28 16:33:59 -0700

    Clean up code
     - Update requirements.txt with pyserial
    
     - Delete convert.py

--- Software/main.py
+++ Software/main.py
@@ -12,41 +12,41 @@ from serial_parser import Serial_Parser
 
 import time
 
+# TO POSSIBLY DELETE
+# def quaternion_to_euler(q):
+#     # Convert quaternion [w,x,y,z] to Euler angles (degrees)
 
-def quaternion_to_euler(q):
-    # Convert quaternion [w,x,y,z] to Euler angles (degrees)
+#     w, x, y, z = q
 
-    w, x, y, z = q
+#     # Roll (x-axis rotation)
+#     sinr_cosp = 2 * (w*x + y*z)
+#     cosr_cosp = 1 - 2*(x*x + y*y)
+#     roll = np.arctan2(
+#         sinr_cosp,
+#         cosr_cosp
+#     )
 
-    # Roll (x-axis rotation)
-    sinr_cosp = 2 * (w*x + y*z)
-    cosr_cosp = 1 - 2*(x*x + y*y)
-    roll = np.arctan2(
-        sinr_cosp,
-        cosr_cosp
-    )
+#     # Pitch (y-axis rotation)
+#     sinp = 2 * (w*y - z*x)
 
-    # Pitch (y-axis rotation)
-    sinp = 2 * (w*y - z*x)
+#     if abs(sinp) >= 1:
+#         pitch = np.sign(sinp) * np.pi/2
+#     else:
+#         pitch = np.arcsin(sinp)
 
-    if abs(sinp) >= 1:
-        pitch = np.sign(sinp) * np.pi/2
-    else:
-        pitch = np.arcsin(sinp)
+#     # Yaw (z-axis rotation)
+#     siny_cosp = 2 * (w*z + x*y)
+#     cosy_cosp = 1 - 2*(y*y + z*z)
+#     yaw = np.arctan2(
+#         siny_cosp,
+#         cosy_cosp
+#     )
 
-    # Yaw (z-axis rotation)
-    siny_cosp = 2 * (w*z + x*y)
-    cosy_cosp = 1 - 2*(y*y + z*z)
-    yaw = np.arctan2(
-        siny_cosp,
-        cosy_cosp
-    )
-
-    return np.degrees([
-        roll,
-        pitch,
-        yaw
-    ])
+#     return np.degrees([
+#         roll,
+#         pitch,
+#         yaw
+#     ])
 
 def run(csv_path = None, sample_rate=None, timestamp_column=None):
     app, window = create_gui()
@@ -68,5 +68,5 @@ def main():
         time.sleep(1)
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
